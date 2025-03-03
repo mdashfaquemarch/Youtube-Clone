8b11cb9ec010ee3ee10c2b399e08b6d99e680d8d
mdashfaquemarch <kashfaque557@gmail.com>
2025-03-04 04:49:54 +0530

    fixes

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,11 +1,25 @@
 class ApiError extends Error {
-    constructor(message, statusCode) {
-      super(message);
-      this.statusCode = statusCode;
-      this.isOperational = true; // Identifies operational errors vs. programming bugs
-      Error.captureStackTrace(this, this.constructor);
-    }
+  constructor(
+      statusCode,
+      message= "Something went wrong",
+      errors = [],
+      stack = ""
+  ){
+      super(message)
+      this.statusCode = statusCode
+      this.data = null
+      this.message = message
+      this.success = false;
+      this.errors = errors
+
+      if (stack) {
+          this.stack = stack
+      } else{
+          Error.captureStackTrace(this, this.constructor)
+      }
+
   }
-  
+}
+
 export {ApiError}
   
