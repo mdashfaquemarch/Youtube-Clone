e86e5018188854569dc840400776b9e7a1445aac
mdashfaquemarch <kashfaque557@gmail.com>
2025-03-03 18:20:20 +0530

    starting user controller

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,24 @@
+import { User } from "../models/users-model.js"
+
+
+const signup = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+const login = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export {
+    signup,
+    login
+}
+
