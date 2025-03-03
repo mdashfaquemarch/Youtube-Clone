47c9c70d14ff119809616a60a0696e7ba4d7218b
mdashfaquemarch <kashfaque557@gmail.com>
2025-03-04 03:49:29 +0530

    minor change in user validation

--- src/utils/validators/user.js
+++ src/utils/validators/user.js
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 const signupValue = Joi.object({
-  username: Joi.string().min(3).max(50).required(),
+  username: Joi.string().min(3).max(50).lowercase().required(),
   fullname: Joi.string().min(3).max(50).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(4).max(40).required()
