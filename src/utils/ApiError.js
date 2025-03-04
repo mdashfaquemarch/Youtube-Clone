90bb723958d1aa6eb3c77606be81fd6260a5a37b
mdashfaquemarch <kashfaque557@gmail.com>
2025-03-04 15:49:31 +0530

    refactor apierror

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,25 +1,24 @@
 class ApiError extends Error {
-  constructor(
-      statusCode,
-      message= "Something went wrong",
-      errors = [],
-      stack = ""
-  ){
-      super(message)
-      this.statusCode = statusCode
-      this.data = null
-      this.message = message
+    constructor(statusCode, message = "Something went wrong", errors = [], stack) {
+      super(message);  // Set message in the built-in Error class
+  
+      this.statusCode = statusCode;
       this.success = false;
-      this.errors = errors
-
+      this.errors = errors;
+      this.data = null; // Optional, can be removed if not used
+  
+      // Capture stack trace only if it's not provided
       if (stack) {
-          this.stack = stack
-      } else{
-          Error.captureStackTrace(this, this.constructor)
+        this.stack = stack;
+      } else {
+        Error.captureStackTrace(this, ApiError);
       }
-
+  
+      // Ensure proper prototype chain (for extending Error)
+      Object.setPrototypeOf(this, ApiError.prototype);
+    }
   }
-}
-
-export {ApiError}
+  
+  export { ApiError };
+  
   
