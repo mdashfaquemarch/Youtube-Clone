d97ed35675ed01586b4c5a942a457cbbc38a9c54
mdashfaquemarch <kashfaque557@gmail.com>
2025-03-05 04:52:46 +0530

    added getVideoById fun

--- src/repositories/video-repository.js
+++ src/repositories/video-repository.js
@@ -6,6 +6,15 @@ class VideoRepo extends CrudRepo {
         super(Video)
     }
 
+    async getVideoById(id) {
+        try {
+            const response = await Video.findById(id).populate("owner", "username email fullname avatar coverImage");
+            return response;
+        } catch (error) {
+            console.error("Something went wrong in the UserRepo : getUser");
+            throw new ApiError(StatusCodes.INTERNAL_SERVER_ERROR, error.message || "Something went wrong");
+        }
+    }
 }
 
 export default VideoRepo;
