6d14899305309d1c1fb867d42075574ad809b5f6
mdashfaquemarch <kashfaque557@gmail.com>
2025-03-04 15:47:39 +0530

    server config added

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -11,7 +11,8 @@ const _config = {
     REFRESH_TOKEN_EXPIRY: process.env.REFRESH_TOKEN_EXPIRY,
     CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
     CLOUDINARY_API_KEY:process.env.CLOUDINARY_API_KEY,
-    CLOUDINARY_API_SECRET:process.env.CLOUDINARY_API_SECRET
+    CLOUDINARY_API_SECRET:process.env.CLOUDINARY_API_SECRET,
+    NODE_ENV: process.env.NODE_ENV
 }
 
 export const config = Object.freeze(_config);
