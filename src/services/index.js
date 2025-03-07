0471bba05512033fc94c80e7c77ce169b0b767ab
mdashfaquemarch <kashfaque557@gmail.com>
2025-03-08 00:29:25 +0530

    importing tweet service

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 import UserService from "./user-service.js";
 import VideoService from "./video-service.js";
+import TweetService from "./tweet-service.js";
 
 export {
     UserService,
-    VideoService
+    VideoService,
+    TweetService
 }
