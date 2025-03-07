a6da2c56bb52efd2e92d0d1929439cc20c0242fc
mdashfaquemarch <kashfaque557@gmail.com>
2025-03-08 00:07:00 +0530

    importing tweet repo

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -1,9 +1,11 @@
 import CrudRepo from "./crud-repository.js";
 import UserRepo from "./user-repository.js";
 import VideoRepo from "./video-repository.js";
+import TweetRepo from "./tweet-repository.js";
 
 export {
     CrudRepo,
     UserRepo,
-    VideoRepo
+    VideoRepo,
+    TweetRepo
 }
