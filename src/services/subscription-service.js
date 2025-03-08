78dac63c6bb8f136e16e6273c215c676f4e58182
mdashfaquemarch <kashfaque557@gmail.com>
2025-03-09 00:04:10 +0530

    toggle subscription done

--- src/services/subscription-service.js
+++ src/services/subscription-service.js
@@ -7,7 +7,35 @@ class SubscriptionService {
         this.serviceRepo = new SubsRepo();
     }
 
+    async toggleSubscription(channelName, userId) {
 
+        const channel = await User.findOne({username: channelName});
+
+        // Check if channel exists
+    if (!channel) {
+        return console.log("Channel not found");
+    }
+
+        if(channel._id.toString() === userId.toString()) {
+           return console.log("user cannot subcribe to itself")
+        }
+   
+        const existingSubscription = await this.serviceRepo.getOneAndDelte({
+           subscriber: userId,
+           subscription: channel._id
+        })
+   
+        if (existingSubscription) {
+            return { statusCode: 200, data: { subscribed: false }, message: "Unsubscribed successfully" };
+        }
+
+        await this.serviceRepo.create({
+            subscriber: userId,
+            subscription: channel._id
+        })
+
+        return { statusCode: 200, data: { subscribed: true }, message: "Subscribed successfully" };
+    }
 }
 
 export default SubscriptionService;
