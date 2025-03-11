26d823f356f2f172a8d69ac6fb225a834dc93aa9
mdashfaquemarch <kashfaque557@gmail.com>
2025-03-11 14:06:48 +0530

    fixing in user service

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -9,96 +9,96 @@ class UserService {
   }
 
   async signup(userData, files) {
-    
-      // checking if user exists or not
 
-      const existedUser = await this.userRepo.checkUserExists(
-        userData.email,
-        userData.username
+    // checking if user exists or not
+
+    const existedUser = await this.userRepo.checkUserExists(
+      userData.email,
+      userData.username
+    );
+
+    if (existedUser) {
+      throw new ApiError(
+        StatusCodes.CONFLICT,
+        "User already exits with this email or username"
       );
+    }
 
-      if (existedUser) {
+    let avatarLocalPath = files?.avatar?.[0].path || null;
+    let coverImageLocalPath = files?.coverImage?.[0].path || null;
+
+    if (!avatarLocalPath) {
+      throw new ApiError(StatusCodes.BAD_REQUEST, "Avatar file is required!");
+    }
+
+    // upload avatar on Cloudinary
+
+    let avatar = null;
+    if (avatarLocalPath) {
+      const avatarFile = files.avatar[0];
+      if (!avatarFile.mimetype.startsWith("image/")) {
         throw new ApiError(
-          StatusCodes.CONFLICT,
-          "User already exits with this email or username"
+          StatusCodes.BAD_REQUEST,
+          "Only image files are allowed for avatar."
         );
       }
 
-      let avatarLocalPath = files?.avatar?.[0].path || null;
-      let coverImageLocalPath = files?.coverImage?.[0].path || null;
-
-      if (!avatarLocalPath) {
-        throw new ApiError(StatusCodes.BAD_REQUEST, "Avatar file is required!");
-      }
-
-      // upload avatar on Cloudinary
-
-      let avatar = null;
-      if (avatarLocalPath) {
-        const avatarFile = files.avatar[0];
-        if (!avatarFile.mimetype.startsWith("image/")) {
-          throw new ApiError(
-            StatusCodes.BAD_REQUEST,
-            "Only image files are allowed for avatar."
-          );
-        }
-
-        try {
-          avatar = await uploadOnCloudinary(avatarLocalPath);
-        } catch (error) {
-          throw new ApiError(
-            StatusCodes.INTERNAL_SERVER_ERROR,
-            "Error while uploading avatar on Cloudinary"
-          );
-        }
-      }
-
-      // upload avatar on Cloudinary
-
-      let coverImage = null;
-      if (coverImageLocalPath) {
-        const coverImageFile = files.coverImage[0];
-        if (!coverImageFile.mimetype.startsWith("image/")) {
-          throw new ApiError(
-            StatusCodes.BAD_REQUEST,
-            "Only image files are allowed for coverImage."
-          );
-        }
-
-        try {
-          coverImage = await uploadOnCloudinary(coverImageLocalPath);
-        } catch (error) {
-          throw new ApiError(
-            StatusCodes.INTERNAL_SERVER_ERROR,
-            "Error while uploading coverImage on Cloudinary"
-          );
-        }
-      }
-
-      if (!avatar.url) {
-        throw new ApiError(StatusCodes.BAD_REQUEST, "Avatar file is required");
-      }
-
-      const user = await this.userRepo.create({
-        username: userData.username.toLowerCase(),
-        email: userData.email,
-        password: userData.password,
-        fullname: userData.fullname,
-        avatar: avatar.url,
-        coverImage: coverImage?.url || "",
-      });
-
-      const createdUser = await this.userRepo.get(user._id);
-
-      if (!createdUser) {
+      try {
+        avatar = await uploadOnCloudinary(avatarLocalPath);
+      } catch (error) {
         throw new ApiError(
           StatusCodes.INTERNAL_SERVER_ERROR,
-          "Something went wrong while registering the user"
+          "Error while uploading avatar on Cloudinary"
+        );
+      }
+    }
+
+    // upload avatar on Cloudinary
+
+    let coverImage = null;
+    if (coverImageLocalPath) {
+      const coverImageFile = files.coverImage[0];
+      if (!coverImageFile.mimetype.startsWith("image/")) {
+        throw new ApiError(
+          StatusCodes.BAD_REQUEST,
+          "Only image files are allowed for coverImage."
         );
       }
 
-      return createdUser;
-    
+      try {
+        coverImage = await uploadOnCloudinary(coverImageLocalPath);
+      } catch (error) {
+        throw new ApiError(
+          StatusCodes.INTERNAL_SERVER_ERROR,
+          "Error while uploading coverImage on Cloudinary"
+        );
+      }
+    }
+
+    if (!avatar.url) {
+      throw new ApiError(StatusCodes.BAD_REQUEST, "Avatar file is required");
+    }
+
+    const user = await this.userRepo.create({
+      username: userData.username.toLowerCase(),
+      email: userData.email,
+      password: userData.password,
+      fullname: userData.fullname,
+      avatar: avatar.url,
+      coverImage: coverImage?.url || "",
+    });
+
+    const createdUser = await this.userRepo.get(user._id);
+
+    if (!createdUser) {
+      throw new ApiError(
+        StatusCodes.INTERNAL_SERVER_ERROR,
+        "Something went wrong while registering the user"
+      );
+    }
+
+    return createdUser;
+
   }
 
   async login(userData) {
@@ -144,21 +144,17 @@ class UserService {
   }
 
   async logout(user) {
-    try {
-      const logout = await this.userRepo.update(user?._id, {
-        $set: {
-          refreshToken: "",
-        },
-      });
-      return logout;
-    } catch (error) {
-      console.log("Something went wrong in the UserService : logout", error);
-      throw new ApiError(
-        StatusCodes.INTERNAL_SERVER_ERROR,
-        "Something went wrong",
-        [error.message]
-      );
+
+    const logout = await this.userRepo.update(user?._id, {
+      $set: {
+        refreshToken: "",
+      },
+    });
+
+    if(!logout) {
+      throw new ApiError(StatusCodes.INTERNAL_SERVER_ERROR, "something went wrong while logging out");
     }
+    return logout;
   }
 }
 
